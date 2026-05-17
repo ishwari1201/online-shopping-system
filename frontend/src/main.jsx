2df5412ceee67fd286f6e95d877d5a270f1a0fc2
ishwari1201 <ishwari.1201@gmail.com>
2026-05-17 22:33:49 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import { ToastContainer } from 'react-toastify'
 import App from './App.jsx'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://online-shopping-system-backend.onrender.com';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
