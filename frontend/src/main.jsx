8659ab494f8bbd774a623798dc72e6114baf5e74
ishwari1201 <ishwari.1201@gmail.com>
2026-05-17 22:17:53 +0530

    Configure axios defaults for API requests
    Set default base URL and credentials for axios.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,10 @@ import './index.css'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
 import App from './App.jsx'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://online-shopping-system-backend.onrender.com';
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
